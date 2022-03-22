20a4886763e9b901f5bd1232fb0fb15b202d1753
vbulash <vbulash@yandex.ru>
2022-03-22 18:08:02 +0300

    Облагораживание LAF + inline CRUD для создания и редактирования практикантов

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -19,169 +19,115 @@
 	<!-- Main content -->
 	<div class="content p-3">
 		<div class="block block-rounded">
-			<div class="block-header block-header-default">
-				<p><span class="required">*</span> - поля, обязательные для заполнения</p>
-				@hasrole('Администратор')
-				<div class="row mb-4">
-					<label class="col-sm-5 col-form-label" for="link">Свяжите данную анкету практиканта с записью пользователя</label>
-					<div class="col-sm-7">
-						<select name="link" id="link">
-							<option selected disabled>Выберите пол</option>
-							<option value="Мужской">Мужской</option>
-							<option value="Женский">Женский</option>
-						</select>
+			<div class="block-header-default">
+				<div class="row p-5">
+					<p><span class="required">*</span> - поля, обязательные для заполнения</p>
+				</div>
+				<div class="block-content p-5">
+					@hasrole('Администратор')
+					<div class="row mb-4">
+						<label class=" col-sm-3 col-form-label" for="link">Свяжите данную анкету практиканта с записью
+							пользователя <span
+								class="required">*</span></label>
+						<div class="col-sm-5 col-form-label">
+							<div class="input-group input-group-lg">
+								<select name="link" id="link" class="form-control select2">
+									<option selected disabled>Выберите пользователя</option>
+									@foreach($users as $key => $value)
+										<option value="{{ $key }}">{{ $value }}</option>
+									@endforeach
+								</select>
+								<span class="input-group-text">
+								<i class="fa fa-chevron-down"></i>
+							</span>
+							</div>
+						</div>
+						@endhasrole
 					</div>
 				</div>
-				@endhasrole
 			</div>
-			<div class="block-content pb-3">
-				<form role="form" class="mb-5" method="post"
-					  action="{{ route('students.store', ['sid' => session()->getId()]) }}"
-					  autocomplete="off" enctype="multipart/form-data">
-					@csrf
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="lastname">Фамилия <span
-								class="required">*</span></label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="lastname" name="lastname">
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="firstname">Имя <span
-								class="required">*</span></label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="firstname" name="firstname">
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="surname">Отчество</label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="surname" name="surname">
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="sex">Пол <span class="required">*</span></label>
-						<div class="col-sm-5">
-							<select name="sex" id="sex">
-								<option selected disabled>Выберите пол</option>
-								<option value="Мужской">Мужской</option>
-								<option value="Женский">Женский</option>
-							</select>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="phone">Телефон <span
-								class="required">*</span></label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="phone" name="phone">
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="email">Электронная почта <span
-								class="required">*</span></label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="email" name="email">
-						</div>
-					</div>
-					<div class="row mb-4 date">
-						<label class="col-sm-3 col-form-label" for="birthdate">Дата рождения <span
-								class="required">*</span></label>
-						<div class="col-sm-5">
-							{{--							<input type="text" class="form-control" id="birthdate" name="birthdate">--}}
-							<input type="text" class="form-control flatpickr-input active" id="birthdate"
-								   name="birthdate" data-date-format="d.m.Y">
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="parents">ФИО родителей, опекунов (до 14 лет), после
-							14 лет можно не указывать</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="parents" id="parents" cols="30" rows="5"></textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="parentscontact">Контактные телефоны родителей или
-							опекунов</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="parentscontact" id="parentscontact" cols="30"
-									  rows="5"></textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="passport">Данные паспорта (серия, номер, кем и когда
-							выдан)</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="passport" id="passport" cols="30" rows="5"></textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="address">Адрес проживания</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="address" id="address" cols="30" rows="5"></textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="institutions">Учебное заведение (на момент
-							заполнения)</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="institutions" id="institutions" cols="30"
-									  rows="5"></textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="grade">Класс / группа (на момент заполнения)</label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="grade" name="grade">
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="hobby">Увлечения (хобби)</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="hobby" id="hobby" cols="30" rows="5"></textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="hobbyyears">Как давно занимается хобби
-							(лет)?</label>
-						<div class="col-sm-5">
-							<input type="number" class="form-control" id="hobbyyears" name="hobbyyears">
-						</div>
-					</div>
-					{{--					<div class="row mb-4 date">--}}
-					{{--						<label class="col-sm-3 col-form-label" for="hobbyachievements">Есть ли достижения, полученные благодаря хобби?</label>--}}
-					{{--						<div class="col-sm-5">--}}
-					{{--							<textarea class="form-control" name="hobbyachievements" id="hobbyachievements" cols="30" rows="10"></textarea>--}}
-					{{--						</div>--}}
-					{{--					</div>--}}
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="contestachievements">Участие в конкурсах,
-							олимпиадах. Достижения</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="contestachievements" id="contestachievements" cols="30"
-									  rows="5"></textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="dream">Чем хочется заниматься в жизни?</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="dream" id="dream" cols="30" rows="5"></textarea>
-						</div>
-					</div>
-					{{--					TODO: реализовать browse_multiple (elFinder?) для хранения документов --}}
-					{{-- $this->crud->field('documents')->label('Документы')->type('browse_multiple'); --}}
+			<form role="form" class="p-5" method="post"
+				  action="{{ route('students.store', ['sid' => session()->getId()]) }}"
+				  autocomplete="off" enctype="multipart/form-data">
+				@csrf
 
+				@php
+					$fields = [
+						['name' => 'lastname', 'title' => 'Фамилия', 'required' => true, 'type' => 'text'],
+						['name' => 'firstname', 'title' => 'Имя', 'required' => true, 'type' => 'text'],
+						['name' => 'surname', 'title' => 'Отчество', 'required' => false, 'type' => 'text'],
+						['name' => 'sex', 'title' => 'Пол', 'required' => true, 'type' => 'select', 'options' => ['Выберите пол', 'Мужской', 'Женский']],
+						['name' => 'birthdate', 'title' => 'Дата рождения', 'required' => true, 'type' => 'date'],
+						['name' => 'phone', 'title' => 'Телефон', 'required' => true, 'type' => 'text'],
+						['name' => 'email', 'title' => 'Электронная почта', 'required' => true, 'type' => 'email'],
+						['name' => 'parents', 'title' => 'ФИО родителей, опекунов (до 14 лет), после 14 лет можно не указывать', 'required' => false, 'type' => 'textarea'],
+						['name' => 'parentscontact', 'title' => 'Контактные телефоны родителей или опекунов', 'required' => false, 'type' => 'textarea'],
+						['name' => 'passport', 'title' => 'Данные паспорта (серия, номер, кем и когда выдан)', 'required' => false, 'type' => 'textarea'],
+						['name' => 'address', 'title' => 'Адрес проживания', 'required' => false, 'type' => 'textarea'],
+						['name' => 'institutions', 'title' => 'Учебное заведение (на момент заполнения)', 'required' => false, 'type' => 'textarea'],
+						['name' => 'grade', 'title' => 'Класс / группа (на момент заполнения)', 'required' => false, 'type' => 'text'],
+						['name' => 'hobby', 'title' => 'Увлечения (хобби)', 'required' => false, 'type' => 'textarea'],
+						['name' => 'hobbyyears', 'title' => 'Как давно занимается хобби (лет)?', 'required' => false, 'type' => 'number'],
+						['name' => 'contestachievements', 'title' => 'Участие в конкурсах, олимпиадах. Достижения', 'required' => false, 'type' => 'textarea'],
+						['name' => 'dream', 'title' => 'Чем хочется заниматься в жизни?', 'required' => false, 'type' => 'textarea'],
+					];
+				@endphp
+
+				@foreach($fields as $field)
 					<div class="row mb-4">
-						<div class="col-sm-3 col-form-label">&nbsp;</div>
+						<label class="col-sm-3 col-form-label" for="{{ $field['name'] }}">{{ $field['title'] }}
+							@if($field['required']) <span class="required">*</span> @endif</label>
 						<div class="col-sm-5">
-							<button type="submit" class="btn btn-primary">Сохранить</button>
-							<a class="btn btn-secondary pl-3"
-							   href="{{ route('students.index', ['sid' => session()->getId()]) }}"
-							   role="button">Закрыть</a>
+							@switch($field['type'])
+
+								@case('text')
+								@case('email')
+								@case('number')
+								<input type="{{ $field['type'] }}" class="form-control" id="{{ $field['name'] }}"
+									   name="{{ $field['name'] }}">
+								@break
+
+								@case('select')
+								<div class="input-group input-group-lg">
+									<select class="form-control select2" name="{{ $field['name'] }}"
+											id="{{ $field['name'] }}">
+										@foreach($field['options'] as $option)
+											<option value="{{ $option }}"
+													@if($loop->first) selected disabled @endif>{{ $option }}</option>
+										@endforeach
+									</select>
+									<span class="input-group-text"><i class="fa fa-chevron-down"></i></span>
+								</div>
+								@break
+
+								@case('date')
+								<input type="text" class="flatpickr-input form-control" id="{{ $field['name'] }}"
+									   name="{{ $field['name'] }}" data-date-format="d.m.Y">
+								@break
+
+								@case('textarea')
+								<textarea class="form-control" name="{{ $field['name'] }}" id="{{ $field['name'] }}"
+										  cols="30"
+										  rows="5"></textarea>
+								@break
+							@endswitch
 						</div>
 					</div>
-				</form>
-			</div>
+				@endforeach
+
+				{{--					TODO: реализовать browse_multiple (elFinder?) для хранения документов --}}
+				{{-- $this->crud->field('documents')->label('Документы')->type('browse_multiple'); --}}
+
+				<div class="row">
+					<div class="col-sm-3 col-form-label">&nbsp;</div>
+					<div class="col-sm-5">
+						<button type="submit" class="btn btn-primary">Сохранить</button>
+						<a class="btn btn-secondary pl-3"
+						   href="{{ route('students.index', ['sid' => session()->getId()]) }}"
+						   role="button">Закрыть</a>
+					</div>
+				</div>
+			</form>
 		</div>
 	</div>
-
 @endsection
--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -22,165 +22,106 @@
 	<div class="content p-3">
 		<div class="block block-rounded">
 			@if(!$show)
-				<div class="block-header block-header-default">
-					<p><span class="required">*</span> - поля, обязательные для заполнения</p>
+				<div class="block-header-default">
+					<div class="row p-5">
+						<p><span class="required">*</span> - поля, обязательные для заполнения</p>
+					</div>
+					<div class="">
+
+					</div>
 				</div>
 			@endif
-			<div class="block-content pb-3">
-				<form role="form" class="mb-5" method="post"
-					  action="{{ route('students.update', ['student' => $student->id, 'sid' => session()->getId()]) }}"
-					  autocomplete="off" enctype="multipart/form-data">
-					@method('PUT')
-					@csrf
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="lastname">Фамилия @if(!$show) <span class="required">*</span> @endif</label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="lastname" name="lastname"
-								   value="{{ $student->lastname }}" @if($show) disabled @endif>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="firstname">Имя @if(!$show) <span class="required">*</span> @endif</label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="firstname" name="firstname"
-								   value="{{ $student->firstname }}" @if($show) disabled @endif>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="surname">Отчество</label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="surname" name="surname"
-								   value="{{ $student->surname }}" @if($show) disabled @endif>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="sex">Пол @if(!$show) <span class="required">*</span> @endif</label>
-						<div class="col-sm-5">
-							<select name="sex" id="sex" @if($show) disabled @endif>
-								<option value="Мужской" @if($student->sex == "Мужской") selected @endif>Мужской</option>
-								<option value="Женский" @if($student->sex == "Женский") selected @endif>Женский</option>
-							</select>
-						</div>
-					</div>
-					<div class="row mb-4 date">
-						<label class="col-sm-3 col-form-label" for="birthdate">Дата рождения @if(!$show) <span class="required">*</span> @endif</label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control flatpickr-input active" id="birthdate" name="birthdate"
-								   data-date-format="d.m.Y" value="{{ $student->birthdate }}" @if($show) disabled @endif>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="phone">Телефон @if(!$show) <span class="required">*</span> @endif</label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="phone" name="phone"
-								   value="{{ $student->phone }}" @if($show) disabled @endif>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="email">Электронная почта @if(!$show) <span class="required">*</span> @endif</label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="email" name="email"
-								   value="{{ $student->email }}" @if($show) disabled @endif>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="parents">ФИО родителей, опекунов (до 14 лет), после
-							14 лет можно не указывать</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="parents" id="parents" cols="30"
-									  rows="5" @if($show) disabled @endif>{{ $student->parents }}</textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="parentscontact">Контактные телефоны родителей или
-							опекунов</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="parentscontact" id="parentscontact" cols="30"
-									  rows="5" @if($show) disabled @endif>{{ $student->parentscontact }}</textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="passport">Данные паспорта (серия, номер, кем и когда
-							выдан)</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="passport" id="passport" cols="30"
-									  rows="5" @if($show) disabled @endif>{{ $student->passport }}</textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="address">Адрес проживания</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="address" id="address" cols="30"
-									  rows="5" @if($show) disabled @endif>{{ $student->address }}</textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="institutions">Учебное заведение (на момент
-							заполнения)</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="institutions" id="institutions" cols="30"
-									  rows="5" @if($show) disabled @endif>{{ $student->institutions }}</textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="grade">Класс / группа (на момент заполнения)</label>
-						<div class="col-sm-5">
-							<input type="text" class="form-control" id="grade" name="grade"
-								   value="{{ $student->grade }}" @if($show) disabled @endif>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="hobby">Увлечения (хобби)</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="hobby" id="hobby" cols="30"
-									  rows="10" @if($show) disabled @endif>{{ $student->hobby }}</textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="hobbyyears">Как давно занимается хобби
-							(лет)?</label>
-						<div class="col-sm-5">
-							<input type="number" class="form-control" id="hobbyyears" name="hobbyyears"
-								   value="{{ $student->hobbyyears }}" @if($show) disabled @endif>
-						</div>
-					</div>
-					{{--					<div class="row mb-4 date">--}}
-					{{--						<label class="col-sm-3 col-form-label" for="hobbyachievements">Есть ли достижения, полученные благодаря хобби?</label>--}}
-					{{--						<div class="col-sm-5">--}}
-					{{--							<textarea class="form-control" name="hobbyachievements" id="hobbyachievements" cols="30" rows="10">{{ $student->hobbyachievements }}</textarea>--}}
-					{{--						</div>--}}
-					{{--					</div>--}}
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="contestachievements">Участие в конкурсах,
-							олимпиадах. Достижения</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="contestachievements" id="contestachievements" cols="30"
-									  rows="5" @if($show) disabled @endif>{{ $student->contestachievements }}</textarea>
-						</div>
-					</div>
-					<div class="row mb-4">
-						<label class="col-sm-3 col-form-label" for="dream">Чем хочется заниматься в жизни?</label>
-						<div class="col-sm-5">
-							<textarea class="form-control" name="dream" id="dream" cols="30"
-									  rows="10" @if($show) disabled @endif>{{ $student->dream }}</textarea>
-						</div>
-					</div>
-					{{--					TODO: реализовать browse_multiple (elFinder?) для хранения документов --}}
-					{{-- $this->crud->field('documents')->label('Документы')->type('browse_multiple'); --}}
 
+			{{-- TODO: привязка пользователя к практиканту по образцу того, что делается в create --}}
+
+			<form role="form" class="p-5" method="post"
+				  action="{{ route('students.update', ['student' => $student->id, 'sid' => session()->getId()]) }}"
+				  autocomplete="off" enctype="multipart/form-data">
+				@method('PUT')
+				@csrf
+
+				@php
+					$fields = [
+						['name' => 'lastname', 'title' => 'Фамилия', 'required' => true, 'type' => 'text', 'value' => $student->lastname],
+						['name' => 'firstname', 'title' => 'Имя', 'required' => true, 'type' => 'text', 'value' => $student->firstname],
+						['name' => 'surname', 'title' => 'Отчество', 'required' => false, 'type' => 'text', 'value' => $student->surname],
+						['name' => 'sex', 'title' => 'Пол', 'required' => true, 'type' => 'select', 'value' => $student->sex, 'options' => ['Мужской', 'Женский']],
+						['name' => 'birthdate', 'title' => 'Дата рождения', 'required' => true, 'type' => 'date', 'value' => $student->birthdate],
+						['name' => 'phone', 'title' => 'Телефон', 'required' => true, 'type' => 'text', 'value' => $student->phone],
+						['name' => 'email', 'title' => 'Электронная почта', 'required' => true, 'type' => 'email', 'value' => $student->email],
+						['name' => 'parents', 'title' => 'ФИО родителей, опекунов (до 14 лет), после 14 лет можно не указывать', 'required' => false, 'type' => 'textarea', 'value' => $student->parents],
+						['name' => 'parentscontact', 'title' => 'Контактные телефоны родителей или опекунов', 'required' => false, 'type' => 'textarea', 'value' => $student->parentscontact],
+						['name' => 'passport', 'title' => 'Данные паспорта (серия, номер, кем и когда выдан)', 'required' => false, 'type' => 'textarea', 'value' => $student->passport],
+						['name' => 'address', 'title' => 'Адрес проживания', 'required' => false, 'type' => 'textarea', 'value' => $student->address],
+						['name' => 'institutions', 'title' => 'Учебное заведение (на момент заполнения)', 'required' => false, 'type' => 'textarea', 'value' => $student->institutions],
+						['name' => 'grade', 'title' => 'Класс / группа (на момент заполнения)', 'required' => false, 'type' => 'text', 'value' => $student->grade],
+						['name' => 'hobby', 'title' => 'Увлечения (хобби)', 'required' => false, 'type' => 'textarea', 'value' => $student->hobby],
+						['name' => 'hobbyyears', 'title' => 'Как давно занимается хобби (лет)?', 'required' => false, 'type' => 'number', 'value' => $student->hobbyyears],
+						['name' => 'contestachievements', 'title' => 'Участие в конкурсах, олимпиадах. Достижения', 'required' => false, 'type' => 'textarea', 'value' => $student->contestachievements],
+						['name' => 'dream', 'title' => 'Чем хочется заниматься в жизни?', 'required' => false, 'type' => 'textarea', 'value' => $student->dream],
+					];
+				@endphp
+
+				@foreach($fields as $field)
 					<div class="row mb-4">
-						<div class="col-sm-3 col-form-label">&nbsp;</div>
+						<label class="col-sm-3 col-form-label" for="{{ $field['name'] }}">{{ $field['title'] }}
+							@if(!$show && $field['required']) <span class="required">*</span> @endif</label>
 						<div class="col-sm-5">
-							@if(!$show)
-								<button type="submit" class="btn btn-primary">Сохранить</button>
-							@endif
-							<a class="btn @if($show) btn-primary @else btn-secondary @endif pl-3"
-							   href="{{ route('students.index', ['sid' => session()->getId()]) }}"
-							   role="button">Закрыть</a>
+							@switch($field['type'])
+
+								@case('text')
+								@case('email')
+								@case('number')
+								<input type="{{ $field['type'] }}" class="form-control" id="{{ $field['name'] }}"
+									   name="{{ $field['name'] }}"
+									   value="{{ $field['value'] }}" @if($show) disabled @endif>
+								@break
+
+								@case('select')
+								<div class="input-group input-group-lg">
+									<select class="form-control select2" name="{{ $field['name'] }}"
+											id="{{ $field['name'] }}" @if($show) disabled @endif>
+										@foreach($field['options'] as $option)
+											<option value="{{ $option }}"
+													@if($field['value'] == $option) selected @endif>{{ $option }}</option>
+										@endforeach
+									</select>
+									<span class="input-group-text"><i class="fa fa-chevron-down"></i></span>
+								</div>
+								@break
+
+								@case('date')
+								<input type="text" class="flatpickr-input form-control" id="{{ $field['name'] }}"
+									   name="{{ $field['name'] }}" data-date-format="d.m.Y"
+									   value="{{ $field['value'] }}"
+									   @if($show) disabled @endif>
+								@break
+
+								@case('textarea')
+								<textarea class="form-control" name="{{ $field['name'] }}" id="{{ $field['name'] }}"
+										  cols="30"
+										  rows="5" @if($show) disabled @endif>{{ $field['value'] }}</textarea>
+								@break
+							@endswitch
 						</div>
 					</div>
-				</form>
-			</div>
+				@endforeach
+
+				{{--					TODO: реализовать browse_multiple (elFinder?) для хранения документов --}}
+				{{-- $this->crud->field('documents')->label('Документы')->type('browse_multiple'); --}}
+
+				<div class="row">
+					<div class="col-sm-3 col-form-label">&nbsp;</div>
+					<div class="col-sm-5">
+						@if(!$show)
+							<button type="submit" class="btn btn-primary">Сохранить</button>
+						@endif
+						<a class="btn @if($show) btn-primary @else btn-secondary @endif pl-3"
+						   href="{{ route('students.index', ['sid' => session()->getId()]) }}"
+						   role="button">Закрыть</a>
+					</div>
+				</div>
+			</form>
 		</div>
 	</div>
 
