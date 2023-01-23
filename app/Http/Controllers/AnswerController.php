472c32489d3b0a1cf0cd60f80c49c409d823b8dc
Валерий Булаш <vbulash@yandex.ru>
2023-01-23 06:17:12 +0300

    Промежуточный коммит

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -2,47 +2,58 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Employer;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class AnswerController extends Controller
-{
-	public function getData(int $employer) {
-		$query = Employer::findOrFail($employer)->orders;
-		// ->wherePivotNotIn('status', [OrderEmployerStatus::REJECTED->value]);
+class AnswerController extends Controller {
+	public function getData(int $employer, int $order) {
+		$query = DB::select(<<<SQL
+SELECT
+    a.id,
+	s.name,
+	os.quantity,
+	a.approved
+FROM
+    answers AS a,
+    orders_specialties AS os,
+    specialties AS s
+WHERE
+    a.orders_specialties_id = os.id
+        AND os.specialty_id = s.id
+		AND os.order_id = :order
+		AND a.employer_id = :employer
+SQL,
+		[
+			'order' => $order,
+			'employer' => $employer
+		]);
 	}
 
-    public function index(int $employer, int $order)
-    {
-        //
-    }
-    public function create()
-    {
-        //
-    }
+	public function index() {
+		//
+	}
+	public function create() {
+		//
+	}
 
-    public function store(Request $request)
-    {
-        //
-    }
+	public function store(Request $request) {
+		//
+	}
 
-    public function show($id)
-    {
-        //
-    }
+	public function show($id) {
+		//
+	}
 
-    public function edit($id)
-    {
-        //
-    }
+	public function edit($id) {
+		//
+	}
 
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	public function update(Request $request, $id) {
+		//
+	}
 
-    public function destroy($id)
-    {
-        //
-    }
+	public function destroy($id) {
+		//
+	}
 }
